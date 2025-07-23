925b6739c108fa52c767a2af5f4348d07a3561d9
LyDinhSon <2351170615@e.tlu.edu.vn>
2025-07-23 16:34:31 +0700

    CRUD JS HTML CSS

---
+++ Temp-HTML-CSS/data.js
@@ -0,0 +1,59 @@
+const Task = [
+  {
+    id: 1,
+    title: "Design web",
+    desc: "abcd",
+    name: "Thomas Hardy",
+    phone: "0171555222",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    priority: "High",
+    status: "undefined"
+  },
+  {
+    id: 1,
+    title: "Dev",
+    desc: "abcd",
+    name: "Thomas Son",
+    phone: "0171555222",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    priority: "High",
+    status: "undefined"
+  },
+  {
+    id: 1,
+    title: "Design web",
+    desc: "abcd",
+    name: "Thomas Hardy",
+    phone: "0171555222",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    priority: "High",
+    status: "undefined"
+  },
+  {
+    id: 1,
+    title: "Design web",
+    desc: "abcd",
+    name: "Thomas Hardy",
+    phone: "0171555222",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    priority: "High",
+    status: "undefined"
+  },
+  {
+    id: 1,
+    title: "Design web",
+    desc: "abcd",
+    name: "Thomas Hardy",
+    phone: "0171555222",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    priority: "High",
+    status: "undefined"
+  }
+];
+
+export default Task;
